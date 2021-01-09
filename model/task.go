8946293e3007bc447a4b89ca762001f57981a98a
Fumiya Sato <fumist1028@gmail.com>
2021-01-09 20:00:30 +0900

    created type of task

--- model/task.go
+++ model/task.go
@@ -1,1 +1,13 @@
 package model
+
+import "time"
+
+type Task struct {
+	Id          int       `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	_           time.Time `json:"created_at"`
+	_           time.Time `json:"updated_at"`
+}
+
+type Tasks []Task
