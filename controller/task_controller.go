2f01aafc0556a919f0aaff6fe4cce5f0d7aa8a68
Fumiya Sato <fumist1028@gmail.com>
2021-01-09 19:58:53 +0900

    created controller

--- controller/task_controller.go
+++ controller/task_controller.go
@@ -1,12 +1,20 @@
 package controller
 
 import (
+	"github.com/fumist23/api-handson/database"
 	"github.com/labstack/echo"
+	"log"
 	"net/http"
 )
 
 func ListTasks(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!あああああ")
+	ctx := c.Request().Context()
+	tasks, err := database.GetList(ctx)
+	if err != nil {
+		log.Printf("error: %v", err)
+		c.String(http.StatusInternalServerError, "サーバーエラー: tasksの取得")
+	}
+	return c.JSON(http.StatusOK, tasks)
 }
 
 func FindById(c echo.Context) error {
